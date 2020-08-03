65227cd7a174cf4d62b04dd92fc21ad0b92581d8
Guto Kuster <gutokuster@gmail.com>
2020-08-03 20:43:45 -0300

    Add models de apps

---
+++ endereco/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Endereco(models.Model):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    descricao = models.CharField(max_length=30, verbose_name='Descrição')
+    endereço = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5, verbose_name='Número')
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+
+    def clean(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=11)
+    ddd = models.CharField(max_length=2)
+    telefone = models.CharField(max_length=11)
+    ddd_whatsapp = models.CharField(max_length=2)
+    whatsapp = models.CharField(max_length=11)
+    email = models.EmailField(max_length=60)
+    data_nascimento = models.DateField()
+    newsletter = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=1,
+        default='A',
+        choices=(
+            ('A', 'Ativo'),
+            ('I', 'Inativo'),
+            ('B', 'Bloqueado'),
+        ),
+    )
+    motivo_bloqueio = models.TextField(max_length=255)
+
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+
+    def clean(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
